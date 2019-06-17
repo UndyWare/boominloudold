58859883cb663367b4f0d8b8138d4f90e9f6a656
Bryton Choates <bchoatestech@gmail.com>
2019-06-16 21:32:02 -0500

    cleanup

--- dt/voice.go
+++ dt/voice.go
@@ -3,7 +3,6 @@ package dt
 import (
 	"fmt"
 	"io"
-	"time"
 
 	dgo "github.com/bwmarrin/discordgo"
 	"github.com/jonas747/dca"
@@ -11,15 +10,11 @@ import (
 
 func loadAudio(path string, vc *dgo.VoiceConnection) error {
 	options := dca.StdEncodeOptions
-	options.RawOutput = true
-	options.Bitrate = 96
-	options.Application = "lowdelay"
 	es, err := dca.EncodeFile(path, options)
 	if err != nil {
 		return err
 	}
 	defer es.Cleanup()
-	time.Sleep(250 * time.Millisecond)
 	if err := vc.Speaking(true); err != nil {
 		return fmt.Errorf("error enabling speaking: %v", err)
 	}
